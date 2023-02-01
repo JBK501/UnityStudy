fd0347cfaafde347483008814a35e3d98f6d432a
BoKyu Jung <apvmfvodls1@naver.com>
2023-02-01 11:45:20 +0900

    유니티 공부내용

---
+++ UI_Base.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public abstract class UI_Base : MonoBehaviour
+{
+    Dictionary<Type, UnityEngine.Object[]> _objects = new Dictionary<Type, UnityEngine.Object[]>();
+
+    public abstract void Init();
+
+    // C# 제네릭 사용 -> C++템플릿같음.
+    // 원하는 컴포넌트를 찾아서 매핑하기 위해 사용한다.
+    protected void Bind<T>(Type type) where T : UnityEngine.Object
+    {
+        string[] names = Enum.GetNames(type);  // Enum값들을 string배열로 받아온다.
+        UnityEngine.Object[] objects = new UnityEngine.Object[name.Length]; // 딕셔너리에 저장할 배열을 만든다.
+        _objects.Add(typeof(T), objects);
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            if (typeof(T) == typeof(GameObject))
+                objects[i] = Util.FindChild(gameObject, names[i], true);
+            else
+                objects[i] = Util.FindChild<T>(gameObject, names[i], true);
+
+            if (objects[i] == null)
+                Debug.Log($"Failed to Bind!{names[i]}");
+        }
+    }
+
+    // 찾은 컴포넌트를 꺼낸다.
+    protected T Get<T>(int idx) where T : UnityEngine.Object
+    {
+        UnityEngine.Object[] objects = null;
+        if (_objects.TryGetValue(typeof(T), out objects) == false)
+            return null;
+
+        // objects[idx]가 가지고 있는 값은 UnityEngine.object타입이다.
+        // 따라서 T타입으로 캐스팅해서 리턴한다.
+        return objects[idx] as T;
+    }
+
+    protected GameObject GetObject(int idx) { return Get<GameObject>(idx); }
+    protected Text GetText(int idx) { return Get<Text>(idx); }
+    protected Button GetButton(int idx) { return Get<Button>(idx); }
+    protected Image GetImage(int idx) { return Get<Image>(idx); }
+
+    public static void BindEvent(GameObject go, Action<PointerEventData> action, Define.UIEvent type = Define.UIEvent.Click)
+    {
+        UI_EventHandler evt = Util.GetOrAddComponent<UI_EventHandler>(go);
+        switch(type)
+        {
+            case Define.UIEvent.Click:
+                evt.OnClickHandler -= action;
+                evt.OnClickHandler += action;
+                break;
+            case Define.UIEvent.Drag:
+                evt.OnDragHandler -= action;
+                evt.OnDragHandler += action;
+                break;
+            default:
+                break;
+        }
+    }
+}
