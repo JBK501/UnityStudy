c5a068049f7720399872d0877854316af1385b27
BoKyu Jung <apvmfvodls1@naver.com>
2023-02-01 12:58:49 +0900

    유니티 UI까지 공부내용

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputManager
+{
+    public Action KeyAction = null;
+    public Action<Define.MouseEvent> MouseAction = null;
+
+    bool _pressed = false;
+
+    public void OnUpdate()
+    {
+        // UI클릭된 상황이면 이동하지 않는다.
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+
+        if (Input.anyKey && KeyAction != null)
+            KeyAction.Invoke();
+
+        if(MouseAction != null)
+        {
+            if(Input.GetMouseButton(0))
+            {
+                MouseAction.Invoke(Define.MouseEvent.Press);
+                _pressed = true;
+            }
+            else
+            {
+                if (_pressed)
+                    MouseAction.Invoke(Define.MouseEvent.Click);
+                _pressed = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// 네트워크, UI, 사운드...
+
+public class Managers : MonoBehaviour
+{
+    //Singleton 디자인패턴 
+    static Managers s_instance; // static키워드 특성상 유일성이 보장된다.
+    static Managers Instance { get { Init(); return s_instance; } }  // 유일한 매니저를 갖고온다.
+
+    InputManager _input = new InputManager();
+    ResourceManager _resource = new ResourceManager();
+    UI_Manager _ui = new UI_Manager();
+
+    public static InputManager Input { get { return Instance._input; } }
+    public static ResourceManager Resource { get { return Instance._resource; } }
+    public static UI_Manager UI { get { return Instance._ui; } }
+    
+
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _input.OnUpdate();
+    }
+
+    static void Init()
+    {
+        if(s_instance == null)
+        {
+            // 초기화
+            GameObject go = GameObject.Find("@Managers");   // @Managers오브젝트를 찾는다.
+
+            if(go == null)  // 못찾았으면
+            {
+                go = new GameObject { name = "@Managers" }; // @Managers오브젝트를 생성한다.
+                go.AddComponent<Managers>();    // Managers컴포넌트를 추가한다.
+            }
+
+            DontDestroyOnLoad(go);
+            s_instance = go.GetComponent<Managers>();
+        }
+
+        
+    }
+}
