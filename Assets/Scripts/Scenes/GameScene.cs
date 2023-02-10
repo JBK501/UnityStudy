33731c89d2a9912035db9b9ac09b49672a62ef6e
BoKyu Jung <apvmfvodls1@naver.com>
2023-02-10 11:17:45 +0900

    ADD Coroutine

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class GameScene : BaseScene
 {
+    // [코루틴]
+    // -> 함수의 상태를 저장/복원 가능하다.
+    //      1. 엄청 오래 걸리는 작업을 잠시 멈출 경우
+    //      2. 원하는 타이밍에 함수를 잠시 멈췄다가 복원하는 경우
+
+    // -> return타입을 우리가 원하는 타입으로 가능하다.(class도 가능)
+
+    Coroutine co;
+
     protected override void Init()
     {
         base.Init();
@@ -12,13 +21,36 @@ public class GameScene : BaseScene
 
         Managers.UI.ShowSceneUI<UI_Inven>();
 
-        for (int i = 0; i < 5; i++)
-            Managers.Resource.Instaniate("UnityChan");
+        // 4초 후에 폭발한다.
+        co = StartCoroutine("ExplodeAfterSeconds", 4.0f);
+
+        // 맘이 바껴서 2초후에 폭발을 멈춘다.
+        StartCoroutine("CoStopExplode", 2.0f);
+    }
+
+    IEnumerator CoStopExplode(float seconds)
+    {
+        Debug.Log("Stop Enter");
+        yield return new WaitForSeconds(seconds);
+        Debug.Log("Stop Execute!!");
+
+        if( co != null)
+        {
+            StopCoroutine(co);
+            co = null;
+        }
+    }
+
+    IEnumerator ExplodeAfterSeconds(float seconds)
+    {
+        Debug.Log("Explode Enter");
+        yield return new WaitForSeconds(seconds);
+        Debug.Log("Explode Execute!!");
+        co = null;
     }
 
     public override void Clear()
     {
-    }
 
-   
+    }
 }
